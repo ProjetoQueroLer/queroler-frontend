bce41eacf9099ce7e52f6656f16fcf06c845363d
leticia.silva <frhleticia@gmail.com>
2026-04-01 14:19:24 -0300

    feat: adiciona header na tela principal

--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -1,7 +1,12 @@
+import { Header } from '@/presentation/shared/components/header/header';
+
 export const Home = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      Tela inicial
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1 flex items-center justify-center">
+        <p className="text-text-secondary">Tela inicial</p>
+      </main>
     </div>
   );
 };
