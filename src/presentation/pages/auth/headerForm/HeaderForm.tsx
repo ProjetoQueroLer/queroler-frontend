68052939c8ad50bb24c496ae50d31351c2235c32
nicofercavv-dev <nicoleferreira281@gmail.com>
2026-04-15 17:11:29 -0300

    feat: adiciona olá na header do form de login

--- src/presentation/pages/auth/headerForm/HeaderForm.tsx
+++ src/presentation/pages/auth/headerForm/HeaderForm.tsx
@@ -1,7 +1,7 @@
 export const HeaderForm = () => (
   <div className="mb-8">
     <h1 className="text-3xl sm:text-4xl font-bold text-white mb-2">
-      Bem-vindo
+      Olá, bem-vindo!
     </h1>
     <p className="text-base lg:text-lg text-text-secondary leading-snug">
       Acesse sua biblioteca pessoal e continue
