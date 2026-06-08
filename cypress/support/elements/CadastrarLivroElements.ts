6d205b3e8ba282a9b2092016ab8bcfbcbac8cdfa
Luciano <lof.fagundes@gmail.com>
2026-06-08 16:57:38 -0300

    feat: adiciona os elementos aos campos no cadastrar de livros

---
+++ cypress/support/elements/CadastrarLivroElements.ts
@@ -0,0 +1,20 @@
+export class CadastrarLivroElements {
+  static readonly buscarLivroInput = '[data-testid="search-input"]';
+  static readonly modalSimButton = '.flex-1.py-2.text-sm.text-white';
+  static readonly logoQueroler = '.w-full.flex.items-center.justify-between';
+  static readonly tituloIncrementeANossaBibliotecaText =
+    '.text-text-primary.text-2xl';
+  static readonly subtituloCadastreUmLivroText = '.text-text-subtitle.text-sm';
+  static readonly imagemLivroInput =
+    '.bg-border-default.border-2.border-dashed.border-border';
+  static readonly isbnInput = '[data-testid="input-isbn"]';
+  static readonly tituloDoLivroInput = '[data-testid="input-titulo"]';
+  static readonly autorInput = '[data-testid="input-autor"]';
+  static readonly editoraInput = '[data-testid="input-editora"]';
+  static readonly anoDePublicacaoInput = '[data-testid="input-ano"]';
+  static readonly numeroDePaginasInput = '[data-testid="input-paginas"]';
+  static readonly idiomaComboBox = '[data-testid="select-idioma"]';
+  static readonly sinopseInput = '[data-testid="input-sinopse"]';
+  static readonly cadastrarLivroButton = '[data-testid="btn-salvar"]';
+  static readonly cancelarCadastroButton = '[data-testid="btn-cancelar"]';
+}
