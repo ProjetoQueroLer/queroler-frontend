88d5052f2d6f72865a0abfef378cf8582a6214d5
Luciano <lof.fagundes@gmail.com>
2026-06-11 15:43:45 -0300

    refactor: padroniza nomenclaturas de ingles para portugues

--- cypress/support/elements/HomeElements.ts
+++ cypress/support/elements/HomeElements.ts
@@ -1,14 +1,15 @@
 export class HomeElements {
-  static readonly userMenuButton = '[data-testid="user-menu-trigger"]';
+  static readonly menuUsuarioTriggerButton =
+    '[data-testid="user-menu-trigger"]';
   static readonly logoutButton = '[data-testid="logout-button"]';
-  static readonly logo = '[alt="Quero Ler"]';
-  static readonly bellIcon = '[data-testid="bell-icon"]';
+  static readonly logoQueroLer = '[alt="Quero Ler"]';
+  static readonly sinoIconButton = '[data-testid="bell-icon"]';
 
   static readonly toast = '.Toastify__toast';
-  static readonly toastCloseButton = '.Toastify__close-button';
+  static readonly fechaToastButton = '.Toastify__close-button';
 
-  static readonly photoUser =
+  static readonly fotoDoUsuarioIcon =
     '.w-8.h-8.rounded-full.bg-brand.flex.items-center.justify-center';
-  static readonly username =
+  static readonly nomeDoUsuarioText =
     '[data-testid="user-menu-trigger"] [class="text-text-primary text-sm hidden lg:block"]';
 }
