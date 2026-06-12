328c3f2444e95bdb9c1a919ec85a3432bafd3e30
Luciano <lof.fagundes@gmail.com>
2026-06-12 17:06:48 -0300

    feat: faker massa de teste no cadastrar de livro na page

--- cypress/support/pages/CadastrarLivroPage.ts
+++ cypress/support/pages/CadastrarLivroPage.ts
@@ -1,8 +1,33 @@
 /// <reference types="cypress" />
 
 import { CadastrarLivroElements } from '../elements/CadastrarLivroElements';
+import { faker } from '@faker-js/faker/locale/pt_BR';
 
+type DadosLivro = {
+  isbn: string;
+  titulo: string;
+  autor: string;
+  editora: string;
+  ano: string;
+  paginas: string;
+  sinopse: string;
+};
+
+const isbnFaker = faker.helpers.arrayElement([
+  faker.string.numeric(10),
+  faker.string.numeric(13),
+]);
 export class CadastrarLivroPage {
+  readonly dadosLivro: DadosLivro = {
+    isbn: isbnFaker,
+    titulo: faker.lorem.words(3),
+    autor: `${faker.person.firstName()} ${faker.person.lastName()}`,
+    editora: faker.company.name(),
+    ano: faker.date.past({ years: 30 }).getFullYear().toString(),
+    paginas: faker.string.numeric(4),
+    sinopse: faker.lorem.sentences(3),
+  };
+
   acessarPaginaCadastrarLivro(texto: string): this {
     cy.get(CadastrarLivroElements.buscarLivroInput).type(texto);
     cy.get(CadastrarLivroElements.modalSimButton).click();
@@ -10,23 +35,60 @@ export class CadastrarLivroPage {
   }
 
   verificarPaginaCarregada(): this {
-    cy.get(CadastrarLivroElements.logoQueroler).should('be.visible');
-    cy.get(CadastrarLivroElements.tituloIncrementeANossaBibliotecaText).should(
-      'be.visible'
+    const elementos = [
+      CadastrarLivroElements.logoQueroler,
+      CadastrarLivroElements.tituloIncrementeANossaBibliotecaText,
+      CadastrarLivroElements.subtituloCadastreUmLivroText,
+      CadastrarLivroElements.isbnInput,
+      CadastrarLivroElements.tituloDoLivroInput,
+      CadastrarLivroElements.autorInput,
+      CadastrarLivroElements.editoraInput,
+      CadastrarLivroElements.anoDePublicacaoInput,
+      CadastrarLivroElements.numeroDePaginasInput,
+      CadastrarLivroElements.idiomaComboBox,
+      CadastrarLivroElements.sinopseInput,
+      CadastrarLivroElements.cadastrarLivroButton,
+      CadastrarLivroElements.cancelarCadastroButton,
+    ];
+
+    elementos.forEach((elemento) => {
+      cy.get(elemento).should('be.visible');
+    });
+
+    return this;
+  }
+
+  private selecionarIdiomaAleatorio(): void {
+    cy.get(CadastrarLivroElements.idiomaComboBox)
+      .find('option')
+      .then((options) => {
+        const valores = [...options]
+          .map((o) => o.value)
+          .filter((v) => v !== '');
+        const idiomaAleatorio = faker.helpers.arrayElement(valores);
+        cy.get(CadastrarLivroElements.idiomaComboBox).select(idiomaAleatorio);
+      });
+  }
+
+  preencherFormularioObrigatorio(): this {
+    cy.get(CadastrarLivroElements.isbnInput).type(this.dadosLivro.isbn);
+    cy.get(CadastrarLivroElements.fechaToastButton)
+      .should('be.visible')
+      .click();
+    cy.get(CadastrarLivroElements.mensagemErrorToastLabel).should('be.visible');
+    cy.get(CadastrarLivroElements.tituloDoLivroInput).type(
+      this.dadosLivro.titulo
     );
-    cy.get(CadastrarLivroElements.subtituloCadastreUmLivroText).should(
-      'be.visible'
+    cy.get(CadastrarLivroElements.autorInput).type(this.dadosLivro.autor);
+    cy.get(CadastrarLivroElements.editoraInput).type(this.dadosLivro.editora);
+    cy.get(CadastrarLivroElements.anoDePublicacaoInput).type(
+      this.dadosLivro.ano
     );
-    cy.get(CadastrarLivroElements.isbnInput).should('be.visible');
-    cy.get(CadastrarLivroElements.tituloDoLivroInput).should('be.visible');
-    cy.get(CadastrarLivroElements.autorInput).should('be.visible');
-    cy.get(CadastrarLivroElements.editoraInput).should('be.visible');
-    cy.get(CadastrarLivroElements.anoDePublicacaoInput).should('be.visible');
-    cy.get(CadastrarLivroElements.numeroDePaginasInput).should('be.visible');
-    cy.get(CadastrarLivroElements.idiomaComboBox).should('be.visible');
-    cy.get(CadastrarLivroElements.sinopseInput).should('be.visible');
-    cy.get(CadastrarLivroElements.cadastrarLivroButton).should('be.visible');
-    cy.get(CadastrarLivroElements.cancelarCadastroButton).should('be.visible');
+    cy.get(CadastrarLivroElements.numeroDePaginasInput).type(
+      this.dadosLivro.paginas
+    );
+    this.selecionarIdiomaAleatorio();
+    cy.get(CadastrarLivroElements.sinopseInput).type(this.dadosLivro.sinopse);
     return this;
   }
 }
