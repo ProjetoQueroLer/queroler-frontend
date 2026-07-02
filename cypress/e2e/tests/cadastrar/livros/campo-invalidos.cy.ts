6858ab3edaacf2432302fef1a86e6c1eceeac98e
Luciano <lof.fagundes@gmail.com>
2026-07-02 17:19:29 -0300

    feat: add validacao campos obrigatorios

--- cypress/e2e/tests/cadastrar/livros/campo-invalidos.cy.ts
+++ cypress/e2e/tests/cadastrar/livros/campo-invalidos.cy.ts
@@ -1,19 +1,28 @@
 /// <reference types="cypress" />
 
 import { CadastrarLivroPage } from '../../../../support/pages/CadastrarLivroPage';
-// import { GeradorDadosLivro } from '../../../../support/utils/geradorDadosLivro';
 
 type TestData = {
   nomeDoLivro: string;
 };
 
-// type Mensagem = {
-//   mensagemToastNoCadastraLivro: {
-
-//   };
-// };
+type Mensagem = {
+  mensagemToastNoCadastraLivro: {
+    ISBNNaoEncontrado: string;
+  };
+  campoObrigatorioCadastrarLivro: {
+    ISBNObrigatorio: string;
+    tituloObrigatorio: string;
+    autorObrigatorio: string;
+    editoraObrigatoria: string;
+    anoPublicacaoObrigatorio: string;
+    numeroDePaginasObrigatorio: string;
+    sinopseObrigatorio: string;
+  };
+};
 
 let dados: TestData;
+let msg: Mensagem;
 
 const cadastrarLivroPage = new CadastrarLivroPage();
 
@@ -22,9 +31,9 @@ describe('Cadastrar Livro', () => {
     cy.fixture('testData').then((fixture) => {
       dados = fixture;
     });
-    // cy.fixture('mensagem').then((fixture) => {
-    //   msg = fixture;
-    // });
+    cy.fixture('mensagem').then((fixture) => {
+      msg = fixture;
+    });
   });
 
   beforeEach(() => {
@@ -39,5 +48,36 @@ describe('Cadastrar Livro', () => {
 
     cadastrarLivroPage.acessarPaginaCadastrarLivro(dados.nomeDoLivro);
     cadastrarLivroPage.botaoSalvarDesativa();
+  });
+
+  it('Deve exibir mensagens de campos obrigatórios ao tentar cadastrar sem preencher o formulário', () => {
+    cy.allure()
+      .feature('Cadastrar Livro')
+      .story('Campos obrigatórios')
+      .severity('normal');
+
+    cadastrarLivroPage.acessarPaginaCadastrarLivro(dados.nomeDoLivro);
+    cadastrarLivroPage.validarOCampoISBNObrigatorio(
+      msg.campoObrigatorioCadastrarLivro.ISBNObrigatorio
+    );
+    cadastrarLivroPage.clicaOsCampos();
+    cadastrarLivroPage.verificarCampoObrigatorio(
+      msg.campoObrigatorioCadastrarLivro.tituloObrigatorio,
+      msg.campoObrigatorioCadastrarLivro.autorObrigatorio,
+      msg.campoObrigatorioCadastrarLivro.editoraObrigatoria,
+      msg.campoObrigatorioCadastrarLivro.anoPublicacaoObrigatorio,
+      msg.campoObrigatorioCadastrarLivro.numeroDePaginasObrigatorio,
+      msg.campoObrigatorioCadastrarLivro.sinopseObrigatorio
+    );
+  });
+
+  it('Deve manter os demais campos obrigatórios desabilitados enquanto o ISBN não for preenchido', () => {
+    cy.allure()
+      .feature('Cadastrar Livro')
+      .story('Campos obrigatórios')
+      .severity('normal');
+
+    cadastrarLivroPage.acessarPaginaCadastrarLivro(dados.nomeDoLivro);
+    cadastrarLivroPage.camposDesabilitados();
   });
 });
